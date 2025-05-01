ebc455499bc134234cafa9fe5379df5d8ecfb133
Tanyaporn <tanyaporn.tong@bumail.net>
2025-05-01 21:25:35 +0700

    Set Shooter

---
+++ Assets/Scripts/Projectile2D.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class Projectile2D : MonoBehaviour
+{
+    [SerializeField] Transform shootPoint;
+    [SerializeField] GameObject target;
+    [SerializeField] Rigidbody2D bulletPrefeb;
+
+  
+    void Update()
+    {
+        
+
+
+
+    }
+}
